f06b82d01ab420f989906db4504eff43f4598c1a
devmamunbd <itsmemamunbd@gmail.com>
2025-02-19 17:11:51 +0600

    according done

--- js/index.js
+++ js/index.js
@@ -29,3 +29,35 @@ let swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+// according
+const accordingContent = document.querySelectorAll(".according-content");
+accordingContent.forEach((item, index) => {
+  let header = item.querySelector("header");
+
+  header.addEventListener("click", () => {
+    item.classList.toggle("open");
+
+    let description = item.querySelector(".according-des");
+    if (item.classList.contains("open")) {
+      description.style.height = `${description.scrollHeight}px`;
+      item.querySelector("i").classList.replace("fa-angle-down", "fa-angle-up");
+    } else {
+      description.style.height = "0px";
+      item.querySelector("i").classList.replace("fa-angle-up", "fa-angle-down");
+    }
+    removeOpen(index);
+  });
+});
+function removeOpen(index1) {
+  accordingContent.forEach((item2, index2) => {
+    if (index1 != index2) {
+      item2.classList.remove("open");
+      let des = item2.querySelector(".according-des");
+      des.style.height = "0px";
+      item2
+        .querySelector("i")
+        .classList.replace("fa-angle-down", "fa-angle-up");
+    }
+  });
+}
